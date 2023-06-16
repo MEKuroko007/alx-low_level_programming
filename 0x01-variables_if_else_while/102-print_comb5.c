20d1cd5f98091373b22ad6bbae4b4895c0f8a6c1
MEKuroko007 <essalhimu@gmail.com>
2023-06-16 02:40:14 -0700

    update task102-fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - entry - point
- * 
- * prints all possible combinations of two two-digit numbers
- * 
+ * prints all possible combinations of two two-digit numbers 
  * Return: Always 0 (Success)
  */
 int main(void)
