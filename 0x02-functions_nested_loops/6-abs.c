e48d7805df687dd669a38344648ea03b935069fb
MEKuroko007 <essalhimu@gmail.com>
2023-06-20 14:42:34 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *	_abs  - function
+ *	@n :number whose to be checked
+ *	Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+		return (-n);
+}
