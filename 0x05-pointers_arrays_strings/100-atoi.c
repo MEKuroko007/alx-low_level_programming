a822bd5e87bd19f854ddc7c421fa2fe69beec9e0
MEKuroko007 <essalhimu@gmail.com>
2023-06-26 12:54:00 -0700

    atoi function fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,29 +4,37 @@
  * _atoi - function that convert a string to an integer.
  * @s: char
  *
- * Return: A integer
+ * Return: an integer
  */
 int _atoi(char *s)
 {
-int sign = 1, number = 0, index = 0;
+int index = 0;
+unsigned int number = 0;
+int sign = 1;
+int is = 0;
 
-if (s[0] == '-')
+while (s[index])
 {
-sign = -1;
-index = 1;
+if (s[index] == 45)
+{
+sign *= -1;
 }
-while (s[index] != '\0')
+
+while (s[index] >= 48 && s[index] <= 57)
 {
-if (s[index] >= '0' && s[index] <= '9')
-{
-number = number * 10 + s[index] - '0';
+is = 1;
+number = (number * 10) + (s[index] - '0');
+index++;
 }
-else
+
+if (is == 1)
 {
 break;
 }
+
 index++;
 }
+
 number *= sign;
 return (number);
 }
