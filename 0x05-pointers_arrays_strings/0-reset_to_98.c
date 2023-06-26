1ef3e5a7484c9780bb185784c396cae6455fd0f3
MEKuroko007 <essalhimu@gmail.com>
2023-06-26 07:27:04 -0700

    some edits

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
  *  reset_to_98 - reset every number to 98
- *  Retun 0 : Succesfull
+ *  @n :  fixed number equal 98
  */
-void reset_to_98(int *n){
-	*n=98;
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
 }
 
