5ba3f67d4a71379f7e4b70dbb6773048c0c25d4e
MEKuroko007 <essalhimu@gmail.com>
2023-07-05 13:04:52 -0700

    compare two string by using recursive function

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,7 +10,7 @@
  */
 int _compare(char *s1, char *s2, int i, int j)
 {
-	if (s1 == NULL && s2 == NULL)
+	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
 	if (s1[i] == s2[j])
 		return (_compare(s1, s2, i + 1, j + 1));
