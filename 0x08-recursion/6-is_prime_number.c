ba7b41381a56d29eefcac357f31bc38e3488a3de
MEKuroko007 <essalhimu@gmail.com>
2023-07-05 07:08:57 -0700

    prime of a number by using recursion function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * check - function that checks to see if number is prime
+ * @n1:int
+ * @n2:int
+ * Return:int
+ */
+int check(int n1, int n2)
+{
+	if (n2 < 2 || n2 % n1 == 0)
+		return (0);
+	else if (n1 > n2 / 2)
+		return (1);
+	else
+		return (check(n1 + 1, n2));
+}
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (check(2, n));
+}
