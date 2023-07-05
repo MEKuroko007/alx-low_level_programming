749d50836d01b077326843e8aa1d1eedbb55d94e
MEKuroko007 <essalhimu@gmail.com>
2023-07-05 06:40:08 -0700

    sqrt of a number by using recursion function

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * check - function checks for the square root
+ * @number:int
+ * @sqrt:int
+ *
+ * Return: int
+ */
+int check(int number, int sqrt)
+{
+	if (number * number == sqrt)
+		return (number);
+	if (number * number >  sqrt)
+		return (-1);
+	return (_recursion(number + 1, sqrt));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (_recursion(1, n));
+}
