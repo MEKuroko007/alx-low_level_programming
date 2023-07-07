3159def8cafd44556ee65a05c99cddac7cc11b26
MEKuroko007 <essalhimu@gmail.com>
2023-07-07 03:12:54 -0700

    modify change.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives.
- * @argc: The number of arguments
- * @av: array of arguments
- * Return: 0
+ * main - Prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
-int main(int argc, char **av)
+int main(int argc, char *argv[])
 {
-	int i;
+	int arg;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	for (arg = 0; arg < argc; arg++)
+		printf("%s\n", argv[arg]);
 
 	return (0);
 }
