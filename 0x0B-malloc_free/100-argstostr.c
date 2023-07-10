3db6cbcfa975abb375d470a42423fde2c50c72f4
MEKuroko007 <essalhimu@gmail.com>
2023-07-10 11:44:15 -0700

    print argu;ents on new line

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * argstostr -  concatenates all the arguments of your program.
+ * @ac:integer
+ * @av:string
+ * Return: a pointer arg
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0, size = 0, j, k;
+	char *arg;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			size++;
+			j++;
+		}
+		size++;
+		i++;
+	}
+	arg = malloc(sizeof(char) * (size + 1));
+	if (arg == NULL)
+		return (NULL);
+	i = 0;
+	for ( ; i < ac ; i++)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			arg[k] = av[i][j];
+
+			j++;
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+	}
+	arg[k] = '\0';
+	return (arg);
+} /* essalhi */
