7afdd34532edab1342895d57e44fcd8196ad0f61
MEKuroko007 <essalhimu@gmail.com>
2023-07-10 11:46:35 -0700

    fixed error of compiler

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * argstostr -  concatenates all the arguments of your program.
  * @ac:integer
@@ -7,7 +8,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i = 0, size = 0, j, k;
+	int i = 0i, size = 0, j, k;
 	char *arg;
 
 	if (ac == 0 || av == NULL)
