c9e1fd599cf206d10493de05159907096834ea3f
MEKuroko007 <essalhimu@gmail.com>
2023-07-19 05:35:18 -0700

    print name for first function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,7 @@
  * print_name(char *name, void (*f)(char *)) - a function that prints a name.
  * @name:string
  * @f:function pointer
+ * Return:void
  */
 
 void print_name(char *name, void (*f)(char *))
