fd7b45ea884a2e9f3d2835667043a0dfd94c2c4f
MEKuroko007 <essalhimu@gmail.com>
2023-07-19 05:31:26 -0700

    print name for first function 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  * print_name(char *name, void (*f)(char *)) - a function that prints a name.
+ * @name:string
+ * @f:function pointer
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(name != NULL && f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 } /* urahara */
