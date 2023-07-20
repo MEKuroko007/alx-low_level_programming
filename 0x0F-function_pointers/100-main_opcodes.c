88c7201ee67234864959add381134b3926aa3517
MEKuroko007 <essalhimu@gmail.com>
2023-07-20 05:19:15 -0700

    fix the errors %02hhx:

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "function_pointers.h"
 /**
  * main - prints its own opcodes
  * @argc: number of arguments
- * @argv: array of arguments
+ * @av: array of arguments
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **av[])
 {
-	int bytes, i;
+	int b, i;
 	char *arr;
 
 	if (argc != 2)
@@ -19,11 +17,24 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	bytes = atoi(argv[1]);
+	b = atoi(av[1]);
 
-	if (bytes < 0)
+	if (b < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
