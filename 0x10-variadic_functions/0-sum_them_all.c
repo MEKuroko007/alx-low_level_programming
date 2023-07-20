3e4f4b251c8d94aa8d38a09d156e9f1300d84729
MEKuroko007 <essalhimu@gmail.com>
2023-07-20 01:32:39 -0700

    sum of numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - calc sum of numbers
+ * @n:args
+ * Return:sum of numbers
+ */
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i = 0 ,sum = 0;
+	va_start(args,n);
+
+	for (; i < n ; i++)
+	{
+		sum += va_arg(args,int)
+	}
+	va_end(args);
+	return sum;
+}
