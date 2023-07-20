aa9a66771d1070761fd115b42ca4d511b3f3be0b
MEKuroko007 <essalhimu@gmail.com>
2023-07-20 05:13:20 -0700

    print sum of numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,15 +1,15 @@
 #include "variadic_functions.h"
+
 /**
- * sum_them_all - calc sum of numbers
- * @n:args
- * Return:sum of numbers
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
+ *
+ * Return:sum
  */
-
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i = 0;
+	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
@@ -17,9 +17,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (; i < n; i++)
-		sum += va_arg(args, int)
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
 
 	va_end(args);
+
 	return (sum);
 }
