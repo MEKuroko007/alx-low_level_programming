58f9b67755874287fc7264621dc2de7394428b32
MEKuroko007 <essalhimu@gmail.com>
2023-07-31 01:19:10 -0700

    readme file created, header file created, task 0 created

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - print data of nodes
+ * @h:pointer to struct node
+ * Return:number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint *temp;
+	int count = 0;
+
+	temp = h;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		count++;
+		temp = temp->n;
+	}
+	return (count);
+}
